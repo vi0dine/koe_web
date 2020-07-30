0b89f5a0850d91c9a40b22e00a65f652b61b9c82
viodine <viodine@yahoo.com>
2020-07-30 15:23:51 +0200

    ResultListItem

--- src/pages/Tests/Tests.page.tsx
+++ src/pages/Tests/Tests.page.tsx
@@ -41,6 +41,44 @@ const TestsPage = () => {
             openTimestamp: Date.now(),
         },
     ];
+    const mockResults = [
+        {
+            id: 1,
+            title: 'Anatomia - Głowa i Szyja',
+            takenAt: Date.now(),
+            approvedBy: {
+                id: 1,
+                name: 'Joanna Czaja',
+            },
+            points: 34,
+            maxPoints: 40,
+            isPassed: true,
+        },
+        {
+            id: 2,
+            title: 'Anatomia - Brzuch i Miednica',
+            takenAt: Date.now(),
+            approvedBy: {
+                id: 1,
+                name: 'Joanna Czaja',
+            },
+            points: 15,
+            maxPoints: 40,
+            isPassed: false,
+        },
+        {
+            id: 3,
+            title: 'Żelazo - wejściówka',
+            takenAt: Date.now(),
+            approvedBy: {
+                id: 2,
+                name: 'Dariusz Ziaja',
+            },
+            points: 21,
+            maxPoints: 100,
+            isPassed: false,
+        },
+    ];
 
     return (
         <div className={'TestsPage__container'}>
@@ -86,13 +124,20 @@ const TestsPage = () => {
                 <div className={'TestsPage__column'}>
                     <span className={'ResultsList__title'}>Recent results</span>
                     <div className={'ResultsList__container'}>
-                        <ResultListItem />
-                        <ResultListItem />
-                        <ResultListItem />
-                        <ResultListItem />
-                        <ResultListItem />
-                        <ResultListItem />
-                        <ResultListItem />
+                        {mockResults.map((result) => {
+                            return (
+                                <ResultListItem
+                                    key={result.id}
+                                    id={result.id}
+                                    title={result.title}
+                                    takenAt={result.takenAt}
+                                    approvedBy={result.approvedBy}
+                                    points={result.points}
+                                    maxPoints={result.maxPoints}
+                                    isPassed={result.isPassed}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
             </div>
--- src/pages/Tests/components/ResultListItem/ResultListItem.component.tsx
+++ src/pages/Tests/components/ResultListItem/ResultListItem.component.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import moment from 'moment';
 import './ResultListItem.styles.scss';
 
-const ResultListItem = () => {
-    return <div className={'ResultListItem__container'}>ITEM 1</div>;
+type ResultListItemProps = {
+    id: number;
+    title: string;
+    takenAt: number;
+    approvedBy: { id: number; name: string };
+    points: number;
+    maxPoints: number;
+    isPassed: boolean;
+};
+
+const ResultListItem = ({ id, title, takenAt, approvedBy, points, maxPoints, isPassed }: ResultListItemProps) => {
+    return (
+        <div className={`ResultListItem__container ${isPassed ? 'passed' : 'failed'}`}>
+            <div className={'ResultListItem__info'}>
+                <span className={'ResultListItem__title'}>{title}</span>
+                <span className={'ResultListItem__takenAt'}>
+                    Taken at: {moment(takenAt).format('DD/MM/YYYY HH:mm')}
+                </span>
+                <span className={'ResultListItem__approvedBy'}>Approved by: {approvedBy.name}</span>
+            </div>
+            <div className={'ResultListItem__results'}>
+                <div className={'ResultListItem__results__container'}>
+                    <span className={'ResultListItem__results__points'}>{points}</span>
+                    <span className={'ResultListItem__results__divider'} />
+                    <span className={'ResultListItem__results__maxPoints'}>{maxPoints}</span>
+                </div>
+            </div>
+            <div className={'ResultListItem__percentage__container'}>
+                <span className={'ResultListItem__percentage__points'}>{Math.round((points / maxPoints) * 100)} %</span>
+            </div>
+        </div>
+    );
 };
 
 export default ResultListItem;
